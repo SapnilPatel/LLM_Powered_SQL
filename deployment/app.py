530970a37a4d05bf4d8b76ba6c31133ceedcfa1d
Sapnil <Sapnilpatel1482@gmail.com>
2023-12-23 14:43:44 +0530

    made necessary changes for deployment

--- deployment/app.py
+++ deployment/app.py
@@ -7,12 +7,11 @@ from langchain.vectorstores import Chroma
 from langchain.prompts import FewShotPromptTemplate
 from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
 from langchain.prompts.prompt import PromptTemplate
-# from few_shots import few_shots
+from few_shots import few_shots
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 def get_few_shot_db_chain():
     
@@ -24,8 +23,28 @@ def get_few_shot_db_chain():
 
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
     
-embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-to_vectorize = [" ".join(example.values()) for example in few_shots]
-vectorstore = Chroma.from_texts(texts=to_vectorize,embedding=embeddings,metadatas=few_shots)
-example_selector = SemanticSimilarityExampleSelector(vectorstore=vectorstore,k=2)
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    to_vectorize = [" ".join(example.values()) for example in few_shots]
+    vectorstore = Chroma.from_texts(texts=to_vectorize,embedding=embeddings,metadatas=few_shots)
+    example_selector = SemanticSimilarityExampleSelector(vectorstore=vectorstore,k=2)
 
+    example_prompt = PromptTemplate(
+    input_variables = ['Question','SQLQuery','SQLResult','Answer'],
+    template = "\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}"
+    )
+    
+    few_shot_prompt = FewShotPromptTemplate(
+    example_selector = example_selector,
+    example_prompt = example_prompt,
+    suffix = PROMPT_SUFFIX,
+    input_variables = ['input','table_info','top_k'] #These variables are used in the prefix and suffix
+    )
+    
+    chain = SQLDatabaseChain.from_llm(llm, db, prompt = few_shot_prompt, verbose=True)
+    
+    return chain
+
+if __name__== "__main__":
+    chain = get_few_shot_db_chain()
+    print()
+    print(chain.run("How many total nike white tshirts are left?"))
