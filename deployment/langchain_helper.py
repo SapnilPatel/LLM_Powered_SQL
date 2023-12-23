9c68eff2b8ebc6a9a4dea75bdd1964e1870582f4
Sapnil <Sapnilpatel1482@gmail.com>
2023-12-23 15:07:36 +0530

    added langchain related code to langchain_helper.py file

--- deployment/langchain_helper.py
+++ deployment/langchain_helper.py
@@ -12,3 +12,39 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def get_few_shot_db_chain():
+    
+    llm = GooglePalm(google_api_key=os.environ["api_key"],temperature=0.1)
+    db_user = "root"
+    db_password = "root"
+    db_host = "localhost"
+    db_name = "sapnil_tshirts"
+
+    db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
+    
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+    to_vectorize = [" ".join(example.values()) for example in few_shots]
+    vectorstore = Chroma.from_texts(texts=to_vectorize,embedding=embeddings,metadatas=few_shots)
+    example_selector = SemanticSimilarityExampleSelector(vectorstore=vectorstore,k=2)
+
+    example_prompt = PromptTemplate(
+    input_variables = ['Question','SQLQuery','SQLResult','Answer'],
+    template = "\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}"
+    )
+    
+    few_shot_prompt = FewShotPromptTemplate(
+    example_selector = example_selector,
+    example_prompt = example_prompt,
+    suffix = PROMPT_SUFFIX,
+    input_variables = ['input','table_info','top_k'] #These variables are used in the prefix and suffix
+    )
+    
+    chain = SQLDatabaseChain.from_llm(llm, db, prompt = few_shot_prompt, verbose=True)
+    
+    return chain
+
+if __name__== "__main__":
+    chain = get_few_shot_db_chain()
+    print()
+    print(chain.run("How many total nike white tshirts are left?"))
