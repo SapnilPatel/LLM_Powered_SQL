63f1f8c162189ba1798a58cc6380496d7b4c72ad
Sapnil <Sapnilpatel1482@gmail.com>
2023-12-23 13:52:48 +0530

    made changes

--- deployment/app.py
+++ deployment/app.py
@@ -23,3 +23,9 @@ def get_few_shot_db_chain():
     db_name = "sapnil_tshirts"
 
     db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
+    
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+to_vectorize = [" ".join(example.values()) for example in few_shots]
+vectorstore = Chroma.from_texts(texts=to_vectorize,embedding=embeddings,metadatas=few_shots)
+example_selector = SemanticSimilarityExampleSelector(vectorstore=vectorstore,k=2)
+
